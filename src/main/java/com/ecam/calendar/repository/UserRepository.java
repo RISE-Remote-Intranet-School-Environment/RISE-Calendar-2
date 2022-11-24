b54430b6ed386c1902e21579b172232f18fa5949
BSalaheddine <78798257+BSalaheddine@users.noreply.github.com>
2022-11-24 22:55:47 +0100

    Add repository for User

---
+++ src/main/java/com/ecam/calendar/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ecam.calendar.repository;
+import com.ecam.calendar.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>  {
+}
+
+
