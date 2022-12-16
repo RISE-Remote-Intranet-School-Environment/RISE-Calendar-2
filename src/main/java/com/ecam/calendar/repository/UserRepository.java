e25da45e2143d91b38d534c3076c70d1b037a34b
elandaloussi20 <78798296+elandaloussi20@users.noreply.github.com>
2022-12-16 16:21:47 +0100

    update lecture model

--- src/main/java/com/ecam/calendar/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ecam.calendar.repository;
-import com.ecam.calendar.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
